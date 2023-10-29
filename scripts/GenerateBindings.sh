177d182cfff95a23c609147fcdab8771b90c646d
Helco <hermann.noll@hotmail.com>
2023-10-29 12:26:39 +0100

    Split options into source and xml for the future

--- scripts/GenerateBindings.sh
+++ scripts/GenerateBindings.sh
@@ -3,6 +3,8 @@
 pushd .
 cd "$(dirname "$0")"
 rm ../SlangNet/Generated/*.cs
+#rm ../SlangNet/GeneratedDoc/*.xml
 rm ../SlangNet.Tests/Generated/*.cs
-ClangSharpPInvokeGenerator @GenerateSlang.rsp
+ClangSharpPInvokeGenerator @GenerateSlangSource.rsp
+#ClangSharpPInvokeGenerator @GenerateSlangXml.rsp
 popd

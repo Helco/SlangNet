c96c2ad2d11f54cf0d535337eaba607c81cbfa2a
Helco <hermann.noll@hotmail.com>
2023-10-30 17:04:59 +0100

    Generate macro constants

--- SlangNet.Bindings/Generated/Slang.cs
+++ SlangNet.Bindings/Generated/Slang.cs
@@ -905,4 +905,91 @@ public static unsafe partial class Slang
     [DllImport("slang", CallingConvention = CallingConvention.Cdecl, EntryPoint = "spGetTranslationUnitSource", ExactSpelling = true)]
     [return: NativeTypeName("const char *")]
     public static extern sbyte* GetTranslationUnitSource([NativeTypeName("SlangCompileRequest *")] ICompileRequest* request, int translationUnitIndex);
+
+    [NativeTypeName("#define SLANG_PROCESSOR_X86 0")]
+    public const int SLANG_PROCESSOR_X86 = 0;
+
+    [NativeTypeName("#define SLANG_FACILITY_WIN_GENERAL 0")]
+    public const int SLANG_FACILITY_WIN_GENERAL = 0;
+
+    [NativeTypeName("#define SLANG_FACILITY_WIN_INTERFACE 4")]
+    public const int SLANG_FACILITY_WIN_INTERFACE = 4;
+
+    [NativeTypeName("#define SLANG_FACILITY_WIN_API 7")]
+    public const int SLANG_FACILITY_WIN_API = 7;
+
+    [NativeTypeName("#define SLANG_FACILITY_BASE 0x200")]
+    public const int SLANG_FACILITY_BASE = 0x200;
+
+    [NativeTypeName("#define SLANG_FACILITY_CORE SLANG_FACILITY_BASE")]
+    public const int SLANG_FACILITY_CORE = 0x200;
+
+    [NativeTypeName("#define SLANG_FACILITY_INTERNAL SLANG_FACILITY_BASE + 1")]
+    public const int SLANG_FACILITY_INTERNAL = 0x200 + 1;
+
+    [NativeTypeName("#define SLANG_FACILITY_EXTERNAL_BASE 0x210")]
+    public const int SLANG_FACILITY_EXTERNAL_BASE = 0x210;
+
+    [NativeTypeName("#define SLANG_OK 0")]
+    public const int SLANG_OK = 0;
+
+    [NativeTypeName("#define SLANG_FAIL SLANG_MAKE_ERROR(SLANG_FACILITY_WIN_GENERAL, 0x4005)")]
+    public const int SLANG_FAIL = unchecked((((int)(0)) << 16) | ((int)(0x4005)) | (int)(0x80000000));
+
+    [NativeTypeName("#define SLANG_E_NOT_IMPLEMENTED SLANG_MAKE_WIN_GENERAL_ERROR(0x4001)")]
+    public const int SLANG_E_NOT_IMPLEMENTED = unchecked((((int)(0)) << 16) | ((int)(0x4001)) | (int)(0x80000000));
+
+    [NativeTypeName("#define SLANG_E_NO_INTERFACE SLANG_MAKE_WIN_GENERAL_ERROR(0x4002)")]
+    public const int SLANG_E_NO_INTERFACE = unchecked((((int)(0)) << 16) | ((int)(0x4002)) | (int)(0x80000000));
+
+    [NativeTypeName("#define SLANG_E_ABORT SLANG_MAKE_WIN_GENERAL_ERROR(0x4004)")]
+    public const int SLANG_E_ABORT = unchecked((((int)(0)) << 16) | ((int)(0x4004)) | (int)(0x80000000));
+
+    [NativeTypeName("#define SLANG_E_INVALID_HANDLE SLANG_MAKE_ERROR(SLANG_FACILITY_WIN_API, 6)")]
+    public const int SLANG_E_INVALID_HANDLE = unchecked((((int)(7)) << 16) | ((int)(6)) | (int)(0x80000000));
+
+    [NativeTypeName("#define SLANG_E_INVALID_ARG SLANG_MAKE_ERROR(SLANG_FACILITY_WIN_API, 0x57)")]
+    public const int SLANG_E_INVALID_ARG = unchecked((((int)(7)) << 16) | ((int)(0x57)) | (int)(0x80000000));
+
+    [NativeTypeName("#define SLANG_E_OUT_OF_MEMORY SLANG_MAKE_ERROR(SLANG_FACILITY_WIN_API, 0xe)")]
+    public const int SLANG_E_OUT_OF_MEMORY = unchecked((((int)(7)) << 16) | ((int)(0xe)) | (int)(0x80000000));
+
+    [NativeTypeName("#define SLANG_E_BUFFER_TOO_SMALL SLANG_MAKE_CORE_ERROR(1)")]
+    public const int SLANG_E_BUFFER_TOO_SMALL = unchecked((((int)(0x200)) << 16) | ((int)(1)) | (int)(0x80000000));
+
+    [NativeTypeName("#define SLANG_E_UNINITIALIZED SLANG_MAKE_CORE_ERROR(2)")]
+    public const int SLANG_E_UNINITIALIZED = unchecked((((int)(0x200)) << 16) | ((int)(2)) | (int)(0x80000000));
+
+    [NativeTypeName("#define SLANG_E_PENDING SLANG_MAKE_CORE_ERROR(3)")]
+    public const int SLANG_E_PENDING = unchecked((((int)(0x200)) << 16) | ((int)(3)) | (int)(0x80000000));
+
+    [NativeTypeName("#define SLANG_E_CANNOT_OPEN SLANG_MAKE_CORE_ERROR(4)")]
+    public const int SLANG_E_CANNOT_OPEN = unchecked((((int)(0x200)) << 16) | ((int)(4)) | (int)(0x80000000));
+
+    [NativeTypeName("#define SLANG_E_NOT_FOUND SLANG_MAKE_CORE_ERROR(5)")]
+    public const int SLANG_E_NOT_FOUND = unchecked((((int)(0x200)) << 16) | ((int)(5)) | (int)(0x80000000));
+
+    [NativeTypeName("#define SLANG_E_INTERNAL_FAIL SLANG_MAKE_CORE_ERROR(6)")]
+    public const int SLANG_E_INTERNAL_FAIL = unchecked((((int)(0x200)) << 16) | ((int)(6)) | (int)(0x80000000));
+
+    [NativeTypeName("#define SLANG_E_NOT_AVAILABLE SLANG_MAKE_CORE_ERROR(7)")]
+    public const int SLANG_E_NOT_AVAILABLE = unchecked((((int)(0x200)) << 16) | ((int)(7)) | (int)(0x80000000));
+
+    [NativeTypeName("#define SLANG_E_TIME_OUT SLANG_MAKE_CORE_ERROR(8)")]
+    public const int SLANG_E_TIME_OUT = unchecked((((int)(0x200)) << 16) | ((int)(8)) | (int)(0x80000000));
+
+    [NativeTypeName("#define SLANG_UUID_ISlangBlob ISlangBlob::getTypeGuid()")]
+    public static readonly SlangUUID SLANG_UUID_ISlangBlob = getTypeGuid();
+
+    [NativeTypeName("#define SLANG_UNBOUNDED_SIZE (~size_t(0))")]
+    public const UIntPtr SLANG_UNBOUNDED_SIZE = (~(nuint)(0));
+
+    [NativeTypeName("#define SLANG_API_VERSION 0")]
+    public const int SLANG_API_VERSION = 0;
+
+    [NativeTypeName("#define SLANG_ERROR_INSUFFICIENT_BUFFER SLANG_E_BUFFER_TOO_SMALL")]
+    public const int SLANG_ERROR_INSUFFICIENT_BUFFER = unchecked((((int)(0x200)) << 16) | ((int)(1)) | (int)(0x80000000));
+
+    [NativeTypeName("#define SLANG_ERROR_INVALID_PARAMETER SLANG_E_INVALID_ARG")]
+    public const int SLANG_ERROR_INVALID_PARAMETER = unchecked((((int)(7)) << 16) | ((int)(0x57)) | (int)(0x80000000));
 }
--- SlangNet.Tests/SmokeTests.cs
+++ SlangNet.Tests/SmokeTests.cs
@@ -9,8 +9,8 @@ internal unsafe class SmokeTests
     public void CanCreateGlobalSession()
     {
         IGlobalSession* globalSession = null;
-        var result = Slang.slang_createGlobalSession(0, &globalSession);
-        Assert.Zero(result);
+        var result = Slang.slang_createGlobalSession(Slang.SLANG_API_VERSION, &globalSession);
+        Assert.That(result, Is.EqualTo(Slang.SLANG_OK));
         Assert.True(globalSession != null, "createGlobalSession returned a null pointer");
         globalSession->release();
     }

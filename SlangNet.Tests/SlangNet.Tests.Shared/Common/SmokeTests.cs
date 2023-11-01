b4907d402ea11bef036749868778b0fc64922eac
Helco <hermann.noll@hotmail.com>
2023-11-01 18:54:47 +0100

    Add test to verify .NET runtime version

--- SlangNet.Tests/SlangNet.Tests.Shared/Common/SmokeTests.cs
+++ SlangNet.Tests/SlangNet.Tests.Shared/Common/SmokeTests.cs
@@ -15,4 +15,24 @@ internal unsafe class SmokeTests
         Assert.True(globalSession != null, "createGlobalSession returned a null pointer");
         globalSession->release();
     }
-}
+
+#if SHOULD_BE_2_0 || SHOULD_BE_2_1
+    [TestCase(typeof(GlobalSession))]
+    [TestCase(typeof(Slang))]
+    public void AssemblyRuntimeIsCorrect(Type checkType)
+    {
+        var runtimeAssembly = checkType.Assembly
+            .GetReferencedAssemblies()
+            .FirstOrDefault(a => a.Name == "netstandard");
+        Assert.NotNull(runtimeAssembly);
+        var runtimeVersion = runtimeAssembly.Version;
+        Assert.NotNull(runtimeVersion);
+        Assert.That(runtimeVersion.Major, Is.EqualTo(2));
+#if SHOULD_BE_2_0
+        Assert.That(runtimeVersion.Minor, Is.EqualTo(0));
+#elif SHOULD_BE_2_1
+        Assert.That(runtimeVersion.Minor, Is.EqualTo(1));
+#endif
+    }
+#endif
+    }

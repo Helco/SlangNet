36dd8829f932b5ea2b484e40e6589225c51a16d2
Helco <hermann.noll@hotmail.com>
2023-11-04 17:36:12 +0100

    Add rest of Module

---
+++ SlangNet.Pretty/Internal/NativeReadOnlyList.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace SlangNet;
+
+internal abstract class NativeReadOnlyList<T> : IReadOnlyList<T>
+    where T : class, IDisposable
+{
+    public abstract int Count { get; }
+    internal abstract SlangResult TryGetAt(int index, [NotNullWhen(true)] out T? value);
+
+    public T this[int index]
+    {
+        get
+        {
+            TryGetAt(index, out var value).ThrowIfFailed();
+            return value!;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    public IEnumerator<T> GetEnumerator() => new Enumerator(this);
+
+    private class Enumerator : IEnumerator<T>
+    {
+        private readonly NativeReadOnlyList<T> list;
+        private readonly int count; // we cache to save native calls
+        private int index = -1;
+
+        public Enumerator(NativeReadOnlyList<T> list)
+        {
+            this.list = list;
+            count = list.Count;
+        }
+
+        public T Current { get; private set; } = null!;
+        object IEnumerator.Current => Current;
+
+        public bool MoveNext()
+        {
+            Current?.Dispose();
+            Current = null!;
+            if (++index >= count)
+                return false;
+            Current = list[index];
+            return true;
+        }
+
+        public void Reset()
+        {
+            Current?.Dispose();
+            Current = null!;
+            index = -1;
+        }
+
+        public void Dispose() { }
+    }
+}
--- SlangNet.Pretty/Module.cs
+++ SlangNet.Pretty/Module.cs
@@ -1,13 +1,75 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using SlangNet.Unsafe;
 
 namespace SlangNet;
 
 public unsafe sealed class Module : ComponentType
 {
+    private readonly EntryPointList entryPoints;
+
     public new IModule* Pointer => (IModule*)base.Pointer;
 
     internal Module(IModule* pointer) : base((IComponentType*)pointer)
     {
+        entryPoints = new(this);
     }
+
+    public SlangResult FindEntryPointByName(ReadOnlySpan<byte> name, [NotNullWhen(true)] out EntryPoint? entryPoint)
+    {
+        IEntryPoint* entryPointPtr = null;
+        SlangResult result;
+        fixed (byte* namePtr = name)
+            result = new(Pointer->findEntryPointByName((sbyte*)namePtr, &entryPointPtr));
+        entryPoint = entryPointPtr == null ? null : new(entryPointPtr);
+        return result;
+    }
+
+    public SlangResult FindEntryPointByName(string name, [NotNullWhen(true)] out EntryPoint? entryPoint)
+    {
+        using var nameStr = new Utf8String(name);
+        IEntryPoint* entryPointPtr = null;
+        var result = Pointer->findEntryPointByName(nameStr, &entryPointPtr);
+        entryPoint = entryPointPtr == null ? null : new(entryPointPtr);
+        return new(result);
+    }
+
+    public EntryPoint GetEntryPointByName(ReadOnlySpan<byte> name)
+    {
+        FindEntryPointByName(name, out var entryPoint).ThrowIfFailed();
+        return entryPoint!;
+    }
+
+    public EntryPoint GetEntryPointByName(string name)
+    {
+        FindEntryPointByName(name, out var entryPoint).ThrowIfFailed();
+        return entryPoint!;
+    }
+
+    private class EntryPointList : NativeReadOnlyList<EntryPoint>
+    {
+        private readonly Module module;
+
+        public EntryPointList(Module module) => this.module = module;
+
+        public override int Count => module.Pointer->getDefinedEntryPointCount();
+
+        internal override SlangResult TryGetAt(int index, [NotNullWhen(true)] out EntryPoint? entryPoint)
+        {
+            IEntryPoint* entryPointPtr = null;
+            var result = module.Pointer->getDefinedEntryPoint(index, &entryPointPtr);
+            entryPoint = entryPointPtr == null ? null : new(entryPointPtr);
+            return new(result);
+        }
+    }
+
+    public IReadOnlyList<EntryPoint> DefinedEntryPoints => entryPoints;
+
+    public SlangResult TryGetDefinedEntryPoint(int index, out EntryPoint? entryPoint) =>
+        entryPoints.TryGetAt(index, out entryPoint);
+
+    public EntryPoint GetDefinedEntryPoint(int index) =>
+        entryPoints[index];
 }

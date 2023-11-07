1d184436c66f3915a63f2d3b323bc7b1262906de
Helco <hermann.noll@hotmail.com>
2023-11-07 16:43:05 +0100

    Add VariableLayoutReflection

---
+++ SlangNet.Pretty/Enums/Stage.cs
@@ -0,0 +1,23 @@
+﻿using static SlangNet.Unsafe.SlangStage;
+
+namespace SlangNet;
+
+public enum Stage : uint
+{
+    None = SLANG_STAGE_NONE,
+    Vertex = SLANG_STAGE_VERTEX,
+    Hull = SLANG_STAGE_HULL,
+    Domain = SLANG_STAGE_DOMAIN,
+    Geometry = SLANG_STAGE_GEOMETRY,
+    Fragment = SLANG_STAGE_FRAGMENT,
+    Compute = SLANG_STAGE_COMPUTE,
+    RayGeneration = SLANG_STAGE_RAY_GENERATION,
+    Intersection = SLANG_STAGE_INTERSECTION,
+    AnyHit = SLANG_STAGE_ANY_HIT,
+    ClosestHit = SLANG_STAGE_CLOSEST_HIT,
+    Miss = SLANG_STAGE_MISS,
+    Callable = SLANG_STAGE_CALLABLE,
+    Mesh = SLANG_STAGE_MESH,
+    Amplification = SLANG_STAGE_AMPLIFICATION,
+    Pixel = SLANG_STAGE_PIXEL,
+}
--- SlangNet.Pretty/VariableLayoutReflection.cs
+++ SlangNet.Pretty/VariableLayoutReflection.cs
@@ -36,4 +36,53 @@ public unsafe readonly struct VariableLayoutReflection : IEquatable<VariableLayo
     public static bool operator ==(VariableLayoutReflection a, VariableLayoutReflection b) => a.pointer == b.pointer;
     public static bool operator !=(VariableLayoutReflection a, VariableLayoutReflection b) => a.pointer != b.pointer;
     public override int GetHashCode() => new IntPtr(pointer).GetHashCode();
+
+    public VariableReflection Variable
+    {
+        get
+        {
+            var ptr = ReflectionVariableLayout_GetVariable(InternalPointer);
+            if (ptr == null)
+                throw new SlangException($"{nameof(ReflectionVariableLayout_GetVariable)} returned a null pointer");
+            return new VariableReflection(ptr);
+        }
+    }
+
+    public TypeLayoutReflection Type
+    {
+        get
+        {
+            var ptr = ReflectionVariableLayout_GetTypeLayout(InternalPointer);
+            if (ptr == null)
+                throw new SlangException($"{nameof(ReflectionVariableLayout_GetTypeLayout)} returned a null pointer");
+            return new TypeLayoutReflection(ptr);
+        }
+    }
+
+    public ulong GetOffset(ParameterCategory category) =>
+        ReflectionVariableLayout_GetOffset(InternalPointer, (SlangParameterCategory)category).ToUInt64();
+
+    public ulong GetSpace(ParameterCategory category) =>
+        ReflectionVariableLayout_GetSpace(InternalPointer, (SlangParameterCategory)category).ToUInt64();
+
+    public string? SemanticName =>
+        InteropUtils.PtrToStringUTF8(ReflectionVariableLayout_GetSemanticName(InternalPointer));
+
+    public ulong SemanticIndex =>
+        ReflectionVariableLayout_GetSemanticIndex(InternalPointer).ToUInt64();
+
+    public Stage Stage =>
+        (Stage)ReflectionVariableLayout_getStage(InternalPointer);
+
+    public VariableLayoutReflection? PendingDataLayout
+    {
+        get
+        {
+            var ptr = ReflectionVariableLayout_getPendingDataLayout(InternalPointer);
+            return ptr == null ? null : new(ptr);
+        }
+    }
+
+    public uint BindingIndex => ReflectionParameter_GetBindingIndex(InternalPointer);
+    public uint BindingSpace => ReflectionParameter_GetBindingSpace(InternalPointer);
 }
